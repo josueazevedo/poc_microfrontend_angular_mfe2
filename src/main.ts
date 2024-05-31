287fef78a5cae232fe1c553c8ed44d7c6c9109e8
Josué Azevedo <josue-azevedo@hotmail.com>
2024-05-31 10:02:56 -0300

    fix: start app from element

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,22 @@ import {
 } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { ApplicationRef, isDevMode } from '@angular/core';
+import { ApplicationRef } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import 'zone.js';
 import { MFE_CONFIG } from './app/config/mfe.config';
 
-if (!isDevMode()) {
+(async () => {
+  const app: ApplicationRef = await createApplication(appConfig);
+
+  const myCustomElement = createCustomElement(AppComponent, {
+    injector: app.injector,
+  });
+  customElements.define(MFE_CONFIG.tag, myCustomElement);
+})();
+
+const run = () => {
   bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
   );
-} else {
-  (async () => {
-    const app: ApplicationRef = await createApplication(appConfig);
-
-    const myCustomElement = createCustomElement(AppComponent, {
-      injector: app.injector,
-    });
-    customElements.define(MFE_CONFIG.tag, myCustomElement);
-  })();
-}
+};
